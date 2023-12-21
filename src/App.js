e1421d2e4b84531fd7fea025c6039438d618a985
Neeraj <krishnaneeraj773@gmail.com>
2023-12-21 23:07:33 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import resumeData from './resumeData'
+import "./App.css";
+import "./fonts.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <header id="home">
+          <div className="row banner">
+            <div className="banner-text">
+              <h1 className="responsive-headline">{resumeData.name}.</h1>
+              <h3 style={{ color: "#fff", fontFamily: "sans-serif " }}>
+                We are a {resumeData.role} <br />
+                {resumeData.roleDescription}
+              </h3>
+              <hr />
+              <ul className="social">
+                {resumeData.socialLinks &&
+                  resumeData.socialLinks.map((item) => {
+                    return (
+                      <li key={item.name}>
+                        <a href={item.url} target="_blank" rel="noreferrer" >
+                          <i className={item.className} />
+                        </a>
+                      </li>
+                    );
+                  })}
+              </ul>
+            </div>
+          </div>
+        </header>
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/resumeData.js
@@ -0,0 +1,21 @@
+let resumeData = {
+  imagebaseurl: "https://github.com/X-Electra/",
+  name: "X-Electra",
+  role: "Tech Community",
+  roleDescription:
+    "Your ultimate tech crew! We thrive on open-source excitement, tech hacks, and coding escapades. Jump into our tech wonderland, where innovation mingles with laughter, and everyone's a coding rockstar! 🚀✨",
+  socialLinks: [
+    {
+      name: "Whatsapp",
+      url: "https://chat.whatsapp.com/BCooOjxxKTlCq5XbXNaHaS",
+      className: "fab fa-whatsapp",
+    },
+    {
+      name: "github",
+      url: "http://github.com/X-Electra",
+      className: "fab fa-github",
+    },
+  ],
+};
+
+export default resumeData;
